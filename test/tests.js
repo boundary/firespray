e71dd0eb6dd31b71f869857925c50a2731fde031
Chris Viau <christopheviau@gmail.com>
2014-10-15 18:05:58 -0400

    Repo structure; first example

---
+++ test/tests.js
@@ -0,0 +1,17 @@
+var expect = chai.expect;
+
+describe('Utilities', function() {
+
+	before(function() {
+
+	});
+
+	after(function() {
+
+	});
+
+	it('should correctly list data', function() {
+		expect(true).to.be.true;
+	});
+
+});
